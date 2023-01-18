e5d134688196711c204fa110cd30307c15577f64
bayupamuji <bayuteguhpamuji@gmail.com>
2023-01-18 14:31:32 +0700

    create web server with web framework hapi

--- notes-backend/src/server.js
+++ notes-backend/src/server.js
@@ -1,1 +1,13 @@
-console.log("Kita akan belajar resful API");
+const Hapi = require("@hapi/hapi");
+
+const init = async () => {
+  const server = Hapi.Server({
+    port: 3000,
+    host: "localhost",
+  });
+
+  await server.start();
+  console.log(`Server running at: ${server.info.uri}`);
+};
+
+init();
