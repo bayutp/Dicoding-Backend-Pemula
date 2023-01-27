43ae681b35f912a6d1a5b64b178484471b16396e
bayupamuji <bayuteguhpamuji@gmail.com>
2023-01-28 06:23:53 +0700

    add delete books api

--- books-backend/src/handler.js
+++ books-backend/src/handler.js
@@ -176,9 +176,32 @@ const updateBookHandler = (request, h) => {
   }
 };
 
+const deleteBookHandler = (request, h) => {
+  const { id } = request.params;
+  const index = books.findIndex((book) => book.id === id);
+
+  if (index !== -1) {
+    books.splice(index, 1);
+    const response = h.response({
+      status: "success",
+      message: "Buku berhasil dihapus",
+    });
+    response.code(200);
+    return response;
+  }
+
+  const response = h.response({
+    status: "fail",
+    message: "Buku gagal dihapus. Id tidak ditemukan",
+  });
+  response.code(404);
+  return response;
+};
+
 module.exports = {
   addBookHandler,
   getAllBooksHandler,
   getBookByIdHandler,
   updateBookHandler,
+  deleteBookHandler,
 };
--- books-backend/src/routes.js
+++ books-backend/src/routes.js
@@ -3,6 +3,7 @@ const {
   getAllBooksHandler,
   getBookByIdHandler,
   updateBookHandler,
+  deleteBookHandler,
 } = require("./handler");
 
 const routes = [
@@ -25,6 +26,11 @@ const routes = [
     method: "PUT",
     path: "/books/{id}",
     handler: updateBookHandler,
+  },
+  {
+    method: "DELETE",
+    path: "/books/{id}",
+    handler: deleteBookHandler,
   },
 ];
 
