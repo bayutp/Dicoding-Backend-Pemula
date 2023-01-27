6e3b8c3d3982d11f68321b04354c7176ce5b95d0
bayupamuji <bayuteguhpamuji@gmail.com>
2023-01-27 20:18:37 +0700

    finished saved books api

--- books-backend/src/handler.js
+++ books-backend/src/handler.js
@@ -1,4 +1,7 @@
-const addBookHanlder = (request, h) => {
+const { nanoid } = require("nanoid");
+const books = require("./books");
+
+const addBookHandler = (request, h) => {
   const {
     name,
     year,
@@ -9,6 +12,66 @@ const addBookHanlder = (request, h) => {
     readPage,
     reading,
   } = request.payload;
+
+  const id = nanoid(16);
+  const finished = pageCount === readPage;
+  const insertedAt = new Date().toISOString;
+  const updatedAt = insertedAt;
+
+  if (name === null || name === "") {
+    const response = h.response({
+      status: "fail",
+      message: "Gagal menambahkan buku. Mohon isi nama buku",
+    });
+    response.code(400);
+    return response;
+  } else if (readPage > pageCount) {
+    const response = h.response({
+      status: "fail",
+      message:
+        "Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount",
+    });
+    response.code(400);
+    return response;
+  } else {
+    const newBook = {
+      id,
+      name,
+      year,
+      author,
+      summary,
+      publisher,
+      pageCount,
+      readPage,
+      finished,
+      reading,
+      insertedAt,
+      updatedAt,
+    };
+
+    books.push(newBook);
+
+    const isSuccessful = books.filter((book) => book.id === id).length > 0;
+
+    if (isSuccessful) {
+      const response = h.response({
+        status: "success",
+        message: "Buku berhasil ditambahkan",
+        data: {
+          bookId: id,
+        },
+      });
+      response.code(201);
+      return response;
+    } else {
+      const response = h.response({
+        status: "error",
+        message: "Buku gagal ditambahkan",
+      });
+      response.code(500);
+      return response;
+    }
+  }
 };
 
-module.exports = addBookHanlder;
+module.exports = { addBookHandler };
--- books-backend/src/server.js
+++ books-backend/src/server.js
@@ -1,10 +1,13 @@
 const Happi = require("@hapi/hapi");
+const { routes } = require("./routes");
 
 const init = async () => {
   const server = Happi.server({
     port: 9000,
     host: "localhost",
   });
+
+  server.route(routes);
 
   await server.start();
   console.log(`Server running at ${server.info.uri}`);
