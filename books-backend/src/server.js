05fce72ad0751a9c7173af66d53261f2ca8817aa
bayupamuji <bayuteguhpamuji@gmail.com>
2023-01-27 18:14:26 +0700

    create web server with hapi framework

--- books-backend/src/server.js
+++ books-backend/src/server.js
@@ -1,1 +1,13 @@
-console.log("testing server");
+const Happi = require("@hapi/hapi");
+
+const init = async () => {
+  const server = Happi.server({
+    port: 9000,
+    host: "localhost",
+  });
+
+  await server.start();
+  console.log(`Server running at ${server.info.uri}`);
+};
+
+init();
